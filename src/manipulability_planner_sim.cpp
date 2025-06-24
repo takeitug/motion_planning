d7905230db6ccfb1ee2d648c31a7b75664b07614
takeitug <nr.ise1999@gmail.com>
2025-06-24 20:35:03 +0900

    pthplanning to start_pos

--- src/manipulability_planner_sim.cpp
+++ src/manipulability_planner_sim.cpp
@@ -191,7 +191,7 @@ int main(int argc, char * argv[])
     // MoveItのセットアップ
     auto moveit_node = rclcpp::Node::make_shared("moveit_commander");
     moveit_node->declare_parameter("robot_name", "lbr");
-    std::string robot_name = moveit_node->get_parameter("robot_name").as_string();
+    std::string robot_name = "iiwa7";
 
     auto move_group_interface = moveit::planning_interface::MoveGroupInterface(
         moveit_node,
