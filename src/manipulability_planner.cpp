7a9c64ff241f5ed7bf3f2a0c8f8660eaaa9a74fa
takeitug <nr.ise1999@gmail.com>
2025-06-24 11:46:11 +0900

    potential calculation

--- src/manipulability_planner.cpp
+++ src/manipulability_planner.cpp
@@ -6,6 +6,8 @@
 #include <std_msgs/msg/float64_multi_array.hpp>
 #include <array>
 
+#include "eigen3/Eigen/Dense"
+
 class ManipulabilityPlanner : public rclcpp::Node
 {
 public:
@@ -41,6 +43,32 @@ public:
     std::vector<double> get_manip_trans() const { return manip_trans_; }
     std::vector<double> get_fk_col4() const { return fk_col4_; }
 
+    std::vector<double> potential(std::vector<double> manip, std::vector<double> current_pos, std::vector<double> goal_pos){
+        std::vector<double> goal_vec=goal_pos-current_pos;
+        double goal_norm=1/sqrt(goal_vec[0]*goal_vec[0]+goal_vec[1]*goal_vec[1]+goal_vec[2]*goal_vec[2]);
+
+        std::vector<double> calc_pos=current_pos;
+        calc_pos[0]=calc_pos[0]+0.01;
+        goal_vec=goal_pos-calc_pos;
+        double goal_x_norm=1/sqrt(goal_vec[0]*goal_vec[0]+goal_vec[1]*goal_vec[1]+goal_vec[2]*goal_vec[2]);
+
+        calc_pos=current_pos;
+        calc_pos[1]=calc_pos[1]+0.01;
+        goal_vec=goal_pos-calc_pos;
+        double goal_y_norm=1/sqrt(goal_vec[0]*goal_vec[0]+goal_vec[1]*goal_vec[1]+goal_vec[2]*goal_vec[2]);
+
+        calc_pos[2]=calc_pos[2]+0.01;
+        goal_vec=goal_pos-calc_pos;
+        double goal_z_norm=1/sqrt(goal_vec[0]*goal_vec[0]+goal_vec[1]*goal_vec[1]+goal_vec[2]*goal_vec[2]);
+
+        std::vector<double> direc(3);
+        direc[0]=goal_x_norm-goal_norm;
+        direc[1]=goal_y_norm-goal_norm;
+        direc[2]=goal_z_norm-goal_norm;
+
+        return -1*direc;
+    }
+
 private:
     double manip_;
     std::vector<double> manip_trans_;
@@ -56,6 +84,9 @@ int main(int argc, char * argv[])
     rclcpp::init(argc, argv);
     auto node = std::make_shared<ManipulabilityPlanner>();
 
+    Eigen::Vector3d goal_pos;
+    goal_pos<<1,0,1;
+
     rclcpp::Rate rate(100);
     while (rclcpp::ok()) {
         // 購読トピックのコールバック実行（値を更新）
@@ -66,11 +97,17 @@ int main(int argc, char * argv[])
         std::vector<double> manip_trans = node->get_manip_trans();
         std::vector<double> fk_col4 = node->get_fk_col4();
 
+        Eigen::Vector3d manip_direc;
+        Eigen::Vector3d current_pos;
+
         std::cout << "[manipulability] " << manip << std::endl;
         std::cout << "[manipulability_trans] [";
         for (size_t i = 0; i < manip_trans.size(); ++i) {
             std::cout << manip_trans[i];
             if (i < manip_trans.size() - 1) std::cout << ", ";
+            if(i<3){
+                manip_direc[i]=manip_trans[i];
+            }
         }
         std::cout << "]" << std::endl;
 
@@ -78,8 +115,13 @@ int main(int argc, char * argv[])
         for (size_t i = 0; i < fk_col4.size(); ++i) {
             std::cout << fk_col4[i];
             if (i < fk_col4.size() - 1) std::cout << ", ";
+            if(i<3){
+                current_pos[i]=fk_col4[i];
+            }
         }
         std::cout << "]" << std::endl;
+
+        
 
         rate.sleep();
     }
